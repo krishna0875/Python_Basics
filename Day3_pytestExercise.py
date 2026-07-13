751b2dc54ab84dd1d71edb09f9e2024d2b35e13a
Jagadeeshgit77 <jagadeesh9665@gmail.com>
2026-07-14 01:10:15 +0530

    my day 3 checks

---
+++ Day3_pytestExercise.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x + y
+    
+def sub(x,y):
+    return x - y
+
+def multi(x, y):
+    return x * y
+
+def zero(x,y):
+    return x + y * 0
+
+def evennum(x,y):
+    return x / y 
