84c89bb7bd9bcedd38b1f51607775a0b15dc4fbe
Jagadeeshgit77 <jagadeesh9665@gmail.com>
2026-07-14 01:26:29 +0530

    Day3 exerecise on pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,23 @@
-from Day3_pytestExercise import add
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from Day3_pytestExercise import add, sub, multi, zero, evennum
+
 
 def test_add():
     assert add(2, 3) == 5
 
-from Day3_pytestExercise import sub
-
 def test_sub():
     assert sub(2, 3) == -1
 
-from Day3_pytestExercise import multi
+
 def test_multi():
     assert multi(2, 3) == 6
 
-from Day3_pytestExercise import zero
+
 def test_zero():
     assert zero(2,3)*0 == 0
 
-from Day3_pytestExercise import evennum
+
 def test_evennum():
     assert evennum (2, 3) 
